00e8c7cdddc8da2c9a9828c6c9c0def635ea4a0f
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2022-08-11 13:29:29 -0300

    add styles to NotFound Component

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,14 @@
+import { ThemeStyled } from '../../contexts/Theme/themeCSS.styles';
+
 const NotFound = () => {
   return (
     <main>
-      <h2>NotFound</h2>
+      <ThemeStyled.PageContentWrapper>
+        <ThemeStyled.Title>Error 404</ThemeStyled.Title>
+        <ThemeStyled.Title>
+          Woops... Looks like this page doesn't exist.
+        </ThemeStyled.Title>
+      </ThemeStyled.PageContentWrapper>
     </main>
   );
 };
